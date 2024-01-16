3714ca14404906868756a13c9e7fc6b1b4ba9b7e
Alice <juhyun.alice.seo@gmail.com>
2024-01-16 15:52:27 +0900

    01-16

---
+++ src/chap_06/_07_VariableScope.java
@@ -0,0 +1,29 @@
+package chap_06;
+
+public class _07_VariableScope {
+    public static void methodA() {
+        //System.out.println(number); 메인 메소드 안에 있는건 메인메소드에서만 호출할수 있음.
+        //System.out.println(result);
+    }
+    public static void methodB() {
+        int result = 1; // 지역변수(메소드 안에서만 쓸수 있음)
+    }
+
+    public static void main(String[] args) {
+        int number = 3;
+        //System.out.println(result);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        //System.out.println(i);
+        {
+            int j = 0;
+            System.out.println(j);
+            System.out.println(number);
+        }
+        //System.out.println(j);
+
+    }
+}
---
+++ src/chap_06/_Quiz_06.java
@@ -0,0 +1,38 @@
+package chap_06;
+
+public class _Quiz_06 {
+    public static String getHiddenData(String data, int index){
+        String result = data.substring(0, index);
+
+        for (int i = 0; i < data.length()-index; i++) {
+            result += "*";
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        /*
+        개인정보를 비공개로 전환하는 메소드 작성
+        하나의 메소드에서 모든 동작 처리
+        각 정보는 아래 위치로부터 비공개 적용
+
+        이름 : 2번째 글자부터 ( 나** )
+        주민등록번호 : 9번째 글자 ( 990130 - 1****** )
+        전화번호 : 10번번째 글자 ( 010 - 1234 - 5678 )
+
+        코드 : 주어진 코드의 getHiddenData()메소드를 완성하시오
+
+        */
+
+        String name = "나코딩" ;
+        String id = "990130-12345678" ;
+        String phone = "010-1234-5678" ;
+
+        System.out.println("이름 : " + getHiddenData(name,1));
+        System.out.println("주민등독번호 : " + getHiddenData(id,8));
+        System.out.println("전화번호 : " + getHiddenData(phone,9));
+
+
+    }
+}
